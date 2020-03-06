162d57b6accc6e33f93a475c55c0ebf058f69777
maarjaryytel <54987539+maarjaryytel@users.noreply.github.com>
2020-03-06 14:56:19 +0200

    Delete mangupall.py

--- mangupall.py
+++
@@ -1,9 +0,0 @@
-def pall():
-    import turtle
-    pall = turtle.Turtle()
-    pall.shape('circle')
-    pall.speed (0)
-    pall.color('red')
-    pall.dx = 3                       #palli kiirus
-    pall.dy = -3
-    pall.penup()                      #kaotab palli liikumise joone
